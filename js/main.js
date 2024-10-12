6692fd2c48202a6ec678c9af45a595f0adf239de
mugahedmotaz <mugahedmotaz@gmail.com>
2024-10-12 13:11:39 +0300

    p;oicy

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ faqs.forEach((faq) => {
         faq.classList.toggle("active");
     })
 })
+
+function showSidebar() {
+ const sidebar = document.querySelector(".sidebar");
+ sidebar.style.display = "flex"
+ const hideIconMenu = document.querySelector(".hideIconMenu")
+ hideIconMenu.style.display = "none";
+
+}
+function hideSidebar() {
+ const sidebar = document.querySelector(".sidebar");
+ sidebar.style.display = "none"
+ const hideIconMenu = document.querySelector(".hideIconMenu")
+ hideIconMenu.style.display = "block";
+}
